154cc3d705460c56fa98c3289f9f207a8f36c557
Leon Dawod <theleondawod1994@hotmail.co.uk>
2016-07-15 21:53:43 +0100

    Add project files.

---
+++ ChauffeurJobManager/login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace ChauffeurJobManager
+{
+    /// <summary>
+    /// Interaction logic for login.xaml
+    /// </summary>
+    public partial class login : Window
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        welcome welcomeWindow = new welcome();
+
+        private void buttonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxUsername.Text.Length == 0) { //If username field is empty
+                errormessage.Text = "Username cannot be empty.";
+                textBoxUsername.Focus(); //Set focus to email textBox
+            }
+        }
+    }
+}
