37ad694e379d4389607de144bcc00b6815105baf
Leon <k1223392@kingston.ac.uk>
2016-08-17 22:35:26 +0100

    Remove all unnecessary namespaces.

--- ChauffeurJobManager/MySQLManager.cs
+++ ChauffeurJobManager/MySQLManager.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data;
 using MySql.Data.MySqlClient;
-using System.Runtime.InteropServices;
 using System.Xml;
-using System.Windows;
 
 namespace ChauffeurJobManager
 {
