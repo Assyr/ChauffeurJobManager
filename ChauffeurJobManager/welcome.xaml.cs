8b2018599d61c8d998380428f4e72bceb79c901f
Leon <k1223392@kingston.ac.uk>
2016-08-21 18:37:37 +0100

    Clean-up some checks.

--- ChauffeurJobManager/welcome.xaml.cs
+++ ChauffeurJobManager/welcome.xaml.cs
@@ -43,13 +43,12 @@ namespace ChauffeurJobManager
 
         public void updateDataGrid()
         {
-            object item = listViewTables.SelectedItem;
-            if (item != null)
+            if (listViewTables.SelectedItem != null)
             {
                 DataTable dataSet = new DataTable();
                 Console.WriteLine(databaseName);
                 welcomeSQLManager.openConnection(databaseName);
-                MySqlDataAdapter dataAdapter = new MySqlDataAdapter("select * from " + databaseName + "." + item.ToString() + ";", welcomeSQLManager.sqlConnect);
+                MySqlDataAdapter dataAdapter = new MySqlDataAdapter("select * from " + databaseName + "." + listViewTables.SelectedItem.ToString() + ";", welcomeSQLManager.sqlConnect);
                 dataAdapter.Fill(dataSet);
                 welcomeSQLManager.closeConnection();
                 dataSet.Columns.RemoveAt(0);
@@ -84,59 +83,48 @@ namespace ChauffeurJobManager
         private void btn_DeleteTable_Click(object sender, RoutedEventArgs e)
         {
             welcomeSQLManager.openConnection(databaseName);
-            object item = listViewTables.SelectedItem;
-            if (item != null)
-            {
-                welcomeSQLManager.sendQueryToDatabase("DROP TABLE IF EXISTS " + databaseName + "." + item.ToString());
-                welcomeSQLManager.closeConnection();
-                selectedTableDataGrid.ItemsSource = null;
-                updateTableList();
-            }
-            else
+            if (listViewTables.SelectedItem == null)
             {
                 MessageBox.Show("Please select a table to delete from the list", "ERROR", MessageBoxButton.OK, MessageBoxImage.Information);
                 return;
             }
+
+            welcomeSQLManager.sendQueryToDatabase("DROP TABLE IF EXISTS " + databaseName + "." + listViewTables.SelectedItem.ToString());
+            welcomeSQLManager.closeConnection();
+            selectedTableDataGrid.ItemsSource = null;
+            updateTableList();
         }
 
         private void btn_openTable_Click(object sender, RoutedEventArgs e)
         {
-            object item = listViewTables.SelectedItem;
-            if (item != null)
+            if (listViewTables.SelectedItem == null)
             {
+                MessageBox.Show("Please select a table to open from the list", "ERROR", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
                 jobManager jobManagerWindow = new jobManager();
 
                 //Populate jobManagerWindow with what we need.
                 jobManagerWindow.txtBlock_tableName.Text = listViewTables.SelectedItem.ToString();
                 jobManagerWindow.tableDatabaseName = databaseName;
-                jobManagerWindow.tableName = item;
+                jobManagerWindow.tableName = listViewTables.SelectedItem;
                 jobManagerWindow.populateJobManagerWindow();
                 jobManagerWindow.populateDataGrid();
                 jobManagerWindow.ShowDialog();
                 updateDataGrid();
-            }
-            else
-            {
-                MessageBox.Show("Please select a table to open from the list", "ERROR", MessageBoxButton.OK, MessageBoxImage.Information);
-                return;
-            }
         }
 
         private void btn_exportToCSV_Click(object sender, RoutedEventArgs e)
         {
-            object Tableitem = listViewTables.SelectedItem;
-            if (Tableitem != null)
-            {
-                exportToCSV CSVExport = new exportToCSV();
-                CSVExport.dg = selectedTableDataGrid;
-                CSVExport.lbl_Header.Content = "Please select the excel template you" + Environment.NewLine + "would like to export " + listViewTables.SelectedItem.ToString() + " table to.";
-                CSVExport.ShowDialog();
-            }
-            else
+            if (listViewTables.SelectedItem == null)
             {
                 MessageBox.Show("Please select a table to export a CSV from", "ERROR", MessageBoxButton.OK, MessageBoxImage.Information);
                 return;
             }
+                exportToCSV CSVExport = new exportToCSV();
+                CSVExport.dg = selectedTableDataGrid;
+                CSVExport.lbl_Header.Content = "Please select the excel template you" + Environment.NewLine + "would like to export " + listViewTables.SelectedItem.ToString() + " table to.";
+                CSVExport.ShowDialog();
         }
     }
 }
